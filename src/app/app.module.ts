23f6f57b1fb4145008788eafd60742b16530bf29
Nicolo7 <nicolocomendador@yahoo.com>
2024-05-24 20:47:42 -0400

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoanComponent } from './components/loan/loan.component';
+import { LandingComponent } from './shared/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoanComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
