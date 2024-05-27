0dea860cde5c0ab570aa8c49d5e74ff8bdde4794
Nicolo7 <nicolocomendador@yahoo.com>
2024-05-27 19:11:13 -0400

    Added prettier to repo

--- src/app/shared/landing/landing.component.ts
+++ src/app/shared/landing/landing.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { LoanAmortizationComponent } from 'src/app/components/loan-amortization/loan-amortization.component';
 import { CommonModule } from '@angular/common';
 
 export enum Calculator {
-  LoanAmortization = 'Loan Amortization Calculator'
+  LoanAmortization = 'Loan Amortization Calculator',
 }
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, LoanAmortizationComponent, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    LoanAmortizationComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
 export class LandingComponent {
   public calculatorType: Calculator = Calculator.LoanAmortization;
